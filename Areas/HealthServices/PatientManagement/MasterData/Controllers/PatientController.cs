027dae588ff8c97f9e52d7ac4d029001627243d3
DevBenari <dev.benarisolusindo@gmail.com>
2026-06-23 15:19:53 +0700

    Update Feature : By Pass Access Permission Akun Kiosk 2

--- Areas/HealthServices/PatientManagement/MasterData/Controllers/PatientController.cs
+++ Areas/HealthServices/PatientManagement/MasterData/Controllers/PatientController.cs
@@ -304,6 +304,7 @@ namespace QuilvianSystemBackend.Areas.HealthServices.PatientManagement.MasterDat
         }
 
         [HttpGet("{id:guid}")]
+        [Authorize(Policy = KioskReadPolicy)]
         [ProducesResponseType(typeof(ApiResponse<PatientDetailResponse>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status404NotFound)]
         [AccessAction(
@@ -313,7 +314,6 @@ namespace QuilvianSystemBackend.Areas.HealthServices.PatientManagement.MasterDat
             AccessType = AccessTypes.Read,
             SortOrder = 1
         )]
-        [AccessPermission("Patient", "Read")]
         public async Task<IActionResult> GetPatientById(Guid id)
         {
             var entity = await BuildBaseQuery()
