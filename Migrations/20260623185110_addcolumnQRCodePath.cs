b1bf492d06547e189df08a9942362394e659922a
andryzainhome <andryzain01@gmail.com>
2026-06-24 01:54:21 +0700

    Update Feature : Adjustment Column Patient for QR Code Path

---
+++ Migrations/20260623185110_addcolumnQRCodePath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuilvianSystemBackend.Migrations
+{
+    public partial class addcolumnQRCodePath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "QrCodePath",
+                schema: "public",
+                table: "MstPatient",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QrCodePath",
+                schema: "public",
+                table: "MstPatient");
+        }
+    }
+}
