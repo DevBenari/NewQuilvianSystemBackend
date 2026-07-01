efc0acdca6b8efd31d1b564a13aba35190af08df
DevBenari <dev.benarisolusindo@gmail.com>
2026-07-01 10:52:42 +0700

    Update Feature : Adjustment Vital Sign 2

---
+++ Migrations/20260701035120_addColumnLingkarBayi.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuilvianSystemBackend.Migrations
+{
+    public partial class addColumnLingkarBayi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "HeadCircumference",
+                schema: "public",
+                table: "TrxPatientVitalSign",
+                type: "numeric(8,2)",
+                precision: 8,
+                scale: 2,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HeadCircumference",
+                schema: "public",
+                table: "TrxPatientVitalSign");
+        }
+    }
+}
