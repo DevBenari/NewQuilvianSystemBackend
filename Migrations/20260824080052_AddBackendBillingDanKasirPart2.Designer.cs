f3d4b8b6db097997f69f3365c8ff94f0dcefc656
yasmina04 <englishrocker07@gmail.com>
2026-08-24 15:26:22 +0700

    fixing seeder

--- Migrations/20260824080052_AddBackendBillingDanKasirPart2.Designer.cs
+++ Migrations/20260824080052_AddBackendBillingDanKasirPart2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QuilvianSystemBackend.Repositories;
@@ -11,9 +12,11 @@ using QuilvianSystemBackend.Repositories;
 namespace QuilvianSystemBackend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260824080052_AddBackendBillingDanKasirPart2")]
+    partial class AddBackendBillingDanKasirPart2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -72652,8 +72655,8 @@ namespace QuilvianSystemBackend.Migrations
 
                     b.Property<string>("ModuleCode")
                         .IsRequired()
-                        .HasMaxLength(250)
-                        .HasColumnType("character varying(250)");
+                        .HasMaxLength(50)
+                        .HasColumnType("character varying(50)");
 
                     b.Property<string>("ModuleName")
                         .IsRequired()
